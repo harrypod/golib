2b1d14483fb862acb8dcde467cb92e84ddfd1ac6
harrypod <harrypodciborski@gmail.com>
2018-09-21 16:23:28 +0100

    Array to String

--- golib.go
+++ golib.go
@@ -159,4 +159,10 @@ func StringRemoveWhitespace (in string) string {
 }
 
 
+func ArrayToString(a []int, delim string) string {
+    return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+    //return strings.Trim(strings.Join(strings.Split(fmt.Sprint(a), " "), delim), "[]")
+    //return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(a)), delim), "[]")
+}
+
 
