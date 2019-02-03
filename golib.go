3e2df4f461da4844f2677a47a9672d49946784ea
harry_pod <harrypodciborski@gmail.com>
2019-02-03 14:46:33 +0000

    Added sleep function

--- golib.go
+++ golib.go
@@ -2,16 +2,17 @@
 // Useful GoLang conversion library
 
 package golib
-import (
-   "strings"
-   "math" 
-   "strconv" 
-   "fmt"
-   "bytes"
-   "encoding/binary"
-   "os"
-)
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 //ParseMath32 : Convert string to 32bit Float
 func ParseMath32(s string) (f float32, err error) {
@@ -20,29 +21,27 @@ func ParseMath32(s string) (f float32, err error) {
 		return 0, fmt.Errorf("Error converting string to float32")
 	}
 	f = math.Float32frombits(uint32(i))
-	return f,nil
+	return f, nil
 }
-
 
 //ParseMath64 : Convert string to 64bit Float
 func ParseMath64(s string) (f float64, err error) {
 	i, err := strconv.ParseUint(s, 16, 64)
 	if err != nil {
-		return 0, fmt.Errorf("Error converting string to float64")	
+		return 0, fmt.Errorf("Error converting string to float64")
 	}
 	f = math.Float64frombits(uint64(i))
-	return f,nil
+	return f, nil
 }
 
 //StrToInt : Convert string to integer
-func StrToInt(s string) (int,error) { 			
-	i,err := strconv.Atoi(s)
-	if err != nil {		
-		return 0, fmt.Errorf("Error converting string to int")	
-	} 
-	return i,nil
+func StrToInt(s string) (int, error) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, fmt.Errorf("Error converting string to int")
+	}
+	return i, nil
 }
-
 
 // DecodeInt attempts to convert a byte array of High/Low Byte values into
 // a 16-bit integer, and returns the result, also with an error, which will
@@ -54,71 +53,66 @@ func DecodeInt(data []byte) (int16, error) {
 	return i, err
 }
 
-
 // GetString Byte to string from hex buffer
-func GetString (in []byte) string  { 
-	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals	
-	for i := 0; i < len(in); i+=2 {			// copied 
-		var hdata  = fmt.Sprintf("%02x",in[i+1])		
-		hexbuffer.WriteString(hdata)	//concat
+func GetString(in []byte) string {
+	var hexbuffer bytes.Buffer        // concatenate x2 pairs of hex vals
+	for i := 0; i < len(in); i += 2 { // copied
+		var hdata = fmt.Sprintf("%02x", in[i+1])
+		hexbuffer.WriteString(hdata) //concat
 	}
 	return hexbuffer.String()
 }
 
-
-
 // GetInt Byte to uint64
-func GetInt (in []byte) (uint64,error)  { 		
-	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals	
-	for i := 0; i < len(in); i+=2 {		
+func GetInt(in []byte) (uint64, error) {
+	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals
+	for i := 0; i < len(in); i += 2 {
 		// take the next two bytes, if available
 		// get hex data out of bytes
-		var hdata  = fmt.Sprintf("%02x",in[i:i+2])
-		hexbuffer.WriteString(hdata)	//concat
+		var hdata = fmt.Sprintf("%02x", in[i:i+2])
+		hexbuffer.WriteString(hdata) //concat
 	}
-	f, err := strconv.ParseUint(hexbuffer.String(), 16, 32)	
-	if err != nil {		
-		return 0, fmt.Errorf("Error converting byte to uint64")	
-	} 
-	return f,nil
+	f, err := strconv.ParseUint(hexbuffer.String(), 16, 32)
+	if err != nil {
+		return 0, fmt.Errorf("Error converting byte to uint64")
+	}
+	return f, nil
 }
-
 
 //GetFloat32 some refernence docs:
 //https://play.golang.org/
 //http://www.perlmonks.org/?node_id=327171
-func GetFloat32 (in []byte) (float32,error) { 		// 32bit encoded IEEE754 
-	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals	
-	for i := 0; i < len(in); i+=2 {		
+func GetFloat32(in []byte) (float32, error) { // 32bit encoded IEEE754
+	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals
+	for i := 0; i < len(in); i += 2 {
 		// take the next two bytes, if available
 		// get hex data out of bytes
-		var hdata  = fmt.Sprintf("%02x",in[i:i+2])		
-		hexbuffer.WriteString(hdata)	//concat
+		var hdata = fmt.Sprintf("%02x", in[i:i+2])
+		hexbuffer.WriteString(hdata) //concat
 	}
 	f, err := ParseMath32(hexbuffer.String())
-	if err != nil {		
-		return 0, fmt.Errorf("Error converting byte to float32")	
-	} 
-	return f,nil
+	if err != nil {
+		return 0, fmt.Errorf("Error converting byte to float32")
+	}
+	return f, nil
 }
 
-// GetFloat64 64bit encoded IEEE754 
+// GetFloat64 64bit encoded IEEE754
 //https://www.h-schmidt.net/FloatConverter/IEEE754.html
-func GetFloat64 (in []byte) (float64,error) { 		
-	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals	
-	for i := 0; i < len(in); i+=2 {		
+func GetFloat64(in []byte) (float64, error) {
+	var hexbuffer bytes.Buffer // concatenate x2 pairs of hex vals
+	for i := 0; i < len(in); i += 2 {
 		// take the next two bytes, if available
 		// get hex data out of bytes
-		var hdata  = fmt.Sprintf("%02x",in[i:i+2])
-		hexbuffer.WriteString(hdata)	//concat
+		var hdata = fmt.Sprintf("%02x", in[i:i+2])
+		hexbuffer.WriteString(hdata) //concat
 	}
 	f, err := ParseMath64(hexbuffer.String())
-	if err != nil {		
-		return 0, fmt.Errorf("Error converting byte to float64")	
-	} 
-	return f,nil
+	if err != nil {
+		return 0, fmt.Errorf("Error converting byte to float64")
+	}
+	return f, nil
 }
-
 
 // Float64ToString convert float64 to string
 func Float64ToString(inputNum float64) string {
@@ -127,48 +121,48 @@ func Float64ToString(inputNum float64) string {
 	return strconv.FormatFloat(inputNum, 'f', 6, 64)
 }
 
-
 // StringToFloat - convert string to float 32
-func StringToFloat (input string) (float32,error) { 	
+func StringToFloat(input string) (float32, error) {
 	value, err := strconv.ParseFloat(input, 32)
 	if err != nil {
-		return 0, fmt.Errorf("Error converting string to float32")	
+		return 0, fmt.Errorf("Error converting string to float32")
 	}
-	return float32(value),nil
+	return float32(value), nil
 }
-
 
 // StringToFloat64 convert String to Float64
-func StringToFloat64(input string)  (float64, error) {     
+func StringToFloat64(input string) (float64, error) {
 	value, err := strconv.ParseFloat(input, 64)
 	if err != nil {
-		return 0, fmt.Errorf("Error converting string to float64")		
+		return 0, fmt.Errorf("Error converting string to float64")
 	}
-	return float64(value),nil
+	return float64(value), nil
 }
 
-
 //StringTrimNull trim \u0000 (NULL) values from end and start of a string
-func StringTrimNull(in[]byte) []byte { 				
-	return bytes.Trim(in[:],"\x00")
-} 
-
+func StringTrimNull(in []byte) []byte {
+	return bytes.Trim(in[:], "\x00")
+}
 
 //StringRemoveWhitespace trim \u0000 (NULL) values from a string
-func StringRemoveWhitespace (in string) string { 
-	return strings.Replace(in, "\u0000", "", -1)			
+func StringRemoveWhitespace(in string) string {
+	return strings.Replace(in, "\u0000", "", -1)
 }
 
 //Array to String - converts [1 2 3 4] to "1,2,3,4"
 func ArrayToString(a []int, delim string) string {
-    return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")    
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
 
 //return true if file exists, false if not
 func FileExists(f string) bool {
-    _, err := os.Stat(f)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return err == nil
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
+
+func sleep(waittime int) {
+	time.Sleep(time.Millisecond * time.Duration(waittime))
 }
